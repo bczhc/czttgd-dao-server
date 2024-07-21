cac46b4b951f8d46b3bdf614df3bece4db45a83f
Zhai Can <bczhc0@126.com>
2024-07-21 16:19:55 +0800

    route: /routes
    This API lists all registered routes

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,20 +1,46 @@
+use std::{fmt, io};
+use std::sync::Mutex;
+
 use axum::response::IntoResponse;
 use axum::Router;
-use axum::routing::get;
 use clap::builder::TypedValueParser;
 use futures::TryStreamExt;
+use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
 
+use crate::mutex_lock;
+
+mod breakpoint;
 pub mod demo;
 mod machines;
 mod users;
-mod breakpoint;
+
+static COLLECTED_ROUTES: Lazy<Mutex<Vec<&'static str>>> =
+    Lazy::new(|| Mutex::new(Default::default()));
+
+macro add_route($router:expr, $t:tt $path:literal, $f:expr) {
+    paste::paste! {
+        $router = $router.route($path, ::axum::routing::[<$t:lower>]($f));
+        mutex_lock!(COLLECTED_ROUTES).push(concat!(stringify!([<$t:upper>]), " ", $path));
+    }
+}
 
 pub fn router() -> Router {
-    Router::new()
-        .route("/stage/:stage/machines", get(machines::machines))
-        .route("/users", get(users::all_users))
-        .route("/break/reasons", get(breakpoint::all_break_reasons))
+    let mut router = Router::new();
+    add_route!(router, GET "/routes", list_routes);
+    add_route!(router, GET "/stage/:stage/machines", machines::machines);
+    add_route!(router, GET "/users", users::all_users);
+    add_route!(router, GET "/break/reasons", breakpoint::all_break_reasons);
+    router
+}
+
+pub async fn list_routes() -> impl IntoResponse {
+    let mut content = String::new();
+    use fmt::Write;
+    for &line in &*mutex_lock!(COLLECTED_ROUTES) {
+        writeln!(&mut content, "{}", line).unwrap();
+    }
+    content
 }
 
 #[derive(Serialize, Deserialize)]
